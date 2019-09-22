12cca2fa973071236e5cf69bcb002bc5d9377c79
Yan Christoffer <yanchristoffer@MacBook-Pro-de-Yan.local>
2019-09-22 18:22:57 -0300

    ïnitial

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,27 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <textarea v-model="message">
+    </textarea>
+    <button @click="submit">Enviar</button>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+        message:''
+    }
+  },
+  methods:{
+    submit(){
+      fetch('https://chatbot-uatu.herokuapp.com/api/message',{
+        method:'POST',
+        body:JSON.stringify({
+          message:this.message
+        })
+      }).then((data)=>data.json()).then((data)=>alert(data.message))
+    }
+  }
+}
+</script>
